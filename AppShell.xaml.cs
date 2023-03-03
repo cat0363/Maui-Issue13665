33f0847b8f0302212059dbd61b3cac784e38d4b5
cat0363 <kenji.nagano.0363@gmail.com>
2023-03-03 11:08:02 +0900

    Repro code of #13665

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_Issue13665;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui_Issue13665;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
